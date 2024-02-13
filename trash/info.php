ff9115ea63dc4b6cc7db2269cbaee270278a94fd
tima <tima.stella88@gmail.com>
2024-02-13 18:42:02 +0200

    -

--- trash/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("sessionAndCoockie.php");
-$email = 'hi';
-$password = $_POST;
-$delim = '|-|';
-$fileInfoWtite = $email . $delim . $password;
-$fileInf = fopen('document.txt', 'a+') or die('Bad not file!');
-fwrite($fileInf, $fileInfoWtite . PHP_EOL);
-fclose($fileInf);
--- trash/path.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-const SITE_ROOT = __DIR__;
-const BASE_URL = 'http://localhost/Study/sessionAndCoockie.php';
-define("ROOT_PATH", realpath(dirname(__FILE__)));
